8482327562a4cde0380e842309a8b4e9d15655fc
pome-ta <pome-ta@users.noreply.github.com>
2021-05-20 16:19:53 +0900

    refacter  main.js

--- public/src/main.js
+++ public/src/main.js
@@ -1,69 +1,16 @@
 'use strict';
 
-/*
-DeviceMotionEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
-    window.addEventListener('devicemotion', (e) => {
-      // do something with e
-      console.log(e);
-    })
-  }
-})
-.catch(console.error)
-*/
-
-/*
-
-DeviceOrientationEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
-    window.addEventListener('deviceorientation', (e) => {
-      // do something with e
-      console.log(e);
-    })
-  }
-})
-.catch(console.error)
-
-*/
-
-/*
-
-if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      // iOS 13+
-      popupOpen("requestPermissionPopup");
-    } else {
-      // non iOS 13+
-      window.addEventListener("deviceorientation", (e) => {
-        console.log('non iOS 13+');
-        console.log(e);
-      });
-    }
-
-
-
-  // for ios13
-  requestPermission = ()=> {
-    DeviceOrientationEvent.requestPermission().then(response => {
-      if (response === 'granted') {
-        window.addEventListener("deviceorientation", (e) => {
-          console.log('for ios13');
-          console.log(e);
-        });
-      }
-    }).catch(console.error);
-  };
-
-*/
-
+/**
+ * ジャイロ着火のおまじない
+ * https://bagelee.com/webar-vr/ios13-webar-webvr/
+ */
 document.getElementById("request_permission").addEventListener("click", function() {
   if (DeviceMotionEvent && DeviceMotionEvent.requestPermission && typeof DeviceMotionEvent.requestPermission === 'function') {
     DeviceMotionEvent.requestPermission();
-    }
+  }
   if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === 'function') {
     DeviceOrientationEvent.requestPermission();
-    }
+  }
 })
 
 
