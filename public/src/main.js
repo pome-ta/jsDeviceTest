49612409339a2114f58e7e048f2bfff3b4886a84
pome-ta <pome-ta@users.noreply.github.com>
2021-05-20 16:13:21 +0900

    main.js

--- public/src/main.js
+++ public/src/main.js
@@ -60,9 +60,10 @@ if (typeof DeviceOrientationEvent.requestPermission === 'function') {
 document.getElementById("request_permission").addEventListener("click", function() {
   if (DeviceMotionEvent && DeviceMotionEvent.requestPermission && typeof DeviceMotionEvent.requestPermission === 'function') {
     DeviceMotionEvent.requestPermission();
-    } if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === 'function') {
-      DeviceOrientationEvent.requestPermission();
-      }
+    }
+  if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === 'function') {
+    DeviceOrientationEvent.requestPermission();
+    }
 })
 
 
