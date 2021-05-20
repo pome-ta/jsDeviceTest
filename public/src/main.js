69b2c8991f5f0ba4a200a8077dfdb86c64cc08e2
pome-ta <pome-ta@users.noreply.github.com>
2021-05-20 16:11:16 +0900

    refacter

--- public/src/main.js
+++ public/src/main.js
@@ -57,21 +57,12 @@ if (typeof DeviceOrientationEvent.requestPermission === 'function') {
 
 */
 
-document.getElementById("request_permission").addEventListener("click", function(){
-if (
-DeviceMotionEvent &&
-DeviceMotionEvent.requestPermission &&
-typeof DeviceMotionEvent.requestPermission === 'function'
-) {
-DeviceMotionEvent.requestPermission();
-}
-if (
-DeviceOrientationEvent &&
-DeviceOrientationEvent.requestPermission &&
-typeof DeviceOrientationEvent.requestPermission === 'function'
-) {
-DeviceOrientationEvent.requestPermission();
-}
+document.getElementById("request_permission").addEventListener("click", function() {
+  if (DeviceMotionEvent && DeviceMotionEvent.requestPermission && typeof DeviceMotionEvent.requestPermission === 'function') {
+    DeviceMotionEvent.requestPermission();
+    } if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === 'function') {
+      DeviceOrientationEvent.requestPermission();
+      }
 })
 
 
